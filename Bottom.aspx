b1cbafe026b5a286a1f7a0c47570e119509e3da7
David Young <davidyoung98@me.com>
2013-09-13 20:02:16 -0500

    initial commit

---
+++ Bottom.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Bottom.aspx.vb" Inherits="ExcelExport.Bottom" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title></title>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <div>
+                <asp:datagrid id="DataGrid1" runat="server" GridLines="Vertical" CellPadding="3" BackColor="White"
+            BorderColor="#999999" BorderWidth="1px" BorderStyle="None" Width="100%" Height="100%" Font-Size="X-Small"
+            Font-Names="Verdana">
+	                <SelectedItemStyle Font-Bold="True" ForeColor="White" BackColor="#008A8C"></SelectedItemStyle>
+	                <AlternatingItemStyle BackColor="Gainsboro"></AlternatingItemStyle>
+	                <ItemStyle BorderWidth="2px" ForeColor="Black" BorderStyle="Solid" BorderColor="Black" BackColor="#EEEEEE"></ItemStyle>
+	                <HeaderStyle Font-Bold="True" HorizontalAlign="Center" BorderWidth="2px" ForeColor="White" BorderStyle="Solid"
+	                BorderColor="Black" BackColor="#000084"></HeaderStyle>
+	                <FooterStyle ForeColor="Black" BackColor="#CCCCCC"></FooterStyle>
+	                <PagerStyle HorizontalAlign="Center" ForeColor="Black" BackColor="#999999" Mode="NumericPages"></PagerStyle>
+                </asp:datagrid>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ Bottom.aspx.vb
@@ -0,0 +1,45 @@
+﻿Public Class Bottom
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' Create a connection and open it.
+        Dim objConn As New System.Data.OleDb.OleDbConnection("provider=microsoft.jet.oledb.4.0; data source=~\Northwind.mdb;")
+        objConn.Open()
+
+        Dim strSQL As String
+        Dim objDataset As New DataSet()
+        Dim objAdapter As New System.Data.OleDb.OleDbDataAdapter()
+
+        ' Get all the customers from the USA.
+        strSQL = "Select * from customers where country='USA'"
+        objAdapter.SelectCommand = New System.Data.OleDb.OleDbCommand(strSQL, objConn)
+        ' Fill the dataset.
+        objAdapter.Fill(objDataset)
+        ' Create a new view.
+        Dim oView As New DataView(objDataset.Tables(0))
+        ' Set up the data grid and bind the data.
+        DataGrid1.DataSource = oView
+        DataGrid1.DataBind()
+
+        ' Verify if the page is to be displayed in Excel.
+        If Request.QueryString("bExcel") = "1" Then
+            ' Set the content type to Excel.
+            Response.ContentType = "application/vnd.ms-excel"
+            ' Remove the charset from the Content-Type header.
+            Response.Charset = ""
+            ' Turn off the view state.
+            Me.EnableViewState = False
+
+            Dim tw As New System.IO.StringWriter()
+            Dim hw As New System.Web.UI.HtmlTextWriter(tw)
+
+            ' Get the HTML for the control.
+            DataGrid1.RenderControl(hw)
+            ' Write the HTML back to the browser.
+            Response.Write(tw.ToString())
+            ' End the response.
+            Response.End()
+        End If
+    End Sub
+
+End Class
